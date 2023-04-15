fafe46cb034fc708aafe51bf2f6aa2165b84098a
Toñi Reina (para FP) <reinaqu.us.es@gmail.com>
2023-04-15 13:12:58 +0200

    Closes #1 Cambiada la ruta del archivo

--- src/fp/ciclismo/test/TestCiclismo.java
+++ src/fp/ciclismo/test/TestCiclismo.java
@@ -13,7 +13,7 @@ public class TestCiclismo {
 	public static void main(String[] args) {
 	  
 		//EstadisticasCarrera est = FactoriaCiclismo.creaEstadisticas2006_16();
-		EstadisticasCarrera est = FactoriaCiclismo.creaEstadisticasDeFichero("Tour Francia","resources/ganadores.txt");
+		EstadisticasCarrera est = FactoriaCiclismo.creaEstadisticasDeFichero("Tour Francia","data/ganadores.txt");
 		mostrarEstadisticas(est);
 
 	}
@@ -23,50 +23,50 @@ public class TestCiclismo {
 		System.out.println((i++)+": Ganadores del tour");
 		System.out.println(est.getGanadores());
 		
-		System.out.println((i++)+": ¿Qué ciclistas han ganado recorriendo menos de 3500 km?");
+		System.out.println((i++)+": Â¿QuÃ© ciclistas han ganado recorriendo menos de 3500 km?");
 		System.out.println(est.getGanadoresConRecorridoInferiorA(3500));
 		
-		System.out.println((i++)+": ¿Cuántos ganadores ha habido?");
+		System.out.println((i++)+": Â¿CuÃ¡ntos ganadores ha habido?");
 		System.out.println(est.getNumeroGanadores());
 		
-		System.out.println((i++)+": ¿Han ganado todos alguna etapa?");
+		System.out.println((i++)+": Â¿Han ganado todos alguna etapa?");
 		System.out.println(est.hanGanadoTodosAlgunaEtapa());
 		
-		System.out.println((i++)+":¿Cuáles son los equipos ganadores?");
+		System.out.println((i++)+":Â¿CuÃ¡les son los equipos ganadores?");
 		System.out.println(est.getEquiposGanadores());
 		
-		System.out.println((i++)+":¿Ha ganado Alberto Contador?");
+		System.out.println((i++)+":Â¿Ha ganado Alberto Contador?");
 		System.out.println(est.buscaGanador("Alberto Contador"));
 		
-		System.out.println((i++)+":¿Ha ganado Alberto Márquez?");
-		System.out.println(est.buscaGanador("Alberto Márquez"));
+		System.out.println((i++)+":Â¿Ha ganado Alberto MÃ¡rquez?");
+		System.out.println(est.buscaGanador("Alberto MÃ¡rquez"));
 		
-		System.out.println((i++)+":¿Quién ganó en 2009?");
+		System.out.println((i++)+":Â¿QuiÃ©n ganÃ³ en 2009?");
 		System.out.println(est.buscaGanador(2009));
 		
-		System.out.println((i++)+":¿Quién ganó en 1999?");
+		System.out.println((i++)+":Â¿QuiÃ©n ganÃ³ en 1999?");
 		System.out.println(est.buscaGanador(1999));
 		
-		System.out.println((i++)+":¿Cuál es el total de kilómetros recorridos en todas las ediciones del tour?");
+		System.out.println((i++)+":Â¿CuÃ¡l es el total de kilÃ³metros recorridos en todas las ediciones del tour?");
 		System.out.println(est.calculaDistanciaTotal());
 		
-		System.out.println((i++)+":¿Cuál es la media de etapas ganadas del equipo Team Sky?");
+		System.out.println((i++)+":Â¿CuÃ¡l es la media de etapas ganadas del equipo Team Sky?");
 		System.out.println(est.getMediaEtapasGanadas("Team Sky"));
 		
-		System.out.println((i++)+":¿Cuántos kilómetros se hicieron en la edición de menor recorrido?");
+		System.out.println((i++)+":Â¿CuÃ¡ntos kilÃ³metros se hicieron en la ediciÃ³n de menor recorrido?");
 		System.out.println(est.getKmMenorRecorrido());
 
-		System.out.println((i++)+":¿Cuál es el ganador que ha alcanzado una mayor velocidad media?");
+		System.out.println((i++)+":Â¿CuÃ¡l es el ganador que ha alcanzado una mayor velocidad media?");
 		System.out.println(est.getGanadorMasRapido());
 
-		System.out.println((i++)+":¿Cuáles son los ganadores, según su nacionalidad?");
+		System.out.println((i++)+":Â¿CuÃ¡les son los ganadores, segÃºn su nacionalidad?");
 		mostrarMapPorLinea(est.getGanadoresPorNacionalidad());
 
-		System.out.println((i++)+":¿Cuántos ganadores hay, según su nacionalidad?");
+		System.out.println((i++)+":Â¿CuÃ¡ntos ganadores hay, segÃºn su nacionalidad?");
 		mostrarMapPorLinea(est.cuentaGanadoresPorNacionalidad());
 
 
-		System.out.println((i++)+":¿Total etapas ganadas por equipo?");
+		System.out.println((i++)+":Â¿Total etapas ganadas por equipo?");
 		mostrarMapPorLinea(est.getTotalEtapasGanadasPorEquipo());
 		
 		String nomFichero1 = "out/ganadores-Spain.txt";
